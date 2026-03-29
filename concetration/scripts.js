ec38c336a711e4c724d9c38cfa3fc573476a6fcb
joshika-gangiredla477 <joshika.gangiredla@wsu.edu>
2026-03-29 00:41:45 +0000

    added randomizer

---
+++ concetration/scripts.js
@@ -0,0 +1,18 @@
+//javascript for interactive concentration game for 1 player
+
+//run this code when the DOM loads
+document.addEventListener("DOMContentLoaded", function(e) {
+
+    //randomize cards
+    let allCards = document.querySelectorAll(".card");
+    let gameboard = document.querySelector = document.querySelector("#");
+
+    //loop through all the cards
+    for (x = 0; x < allCards.length; x++) {
+        let randNum = Math.floor(Math.random() * allCards.length);
+        gameboard.insertBefore(allCards[x], gameboard.children[randNum]);
+        
+    }
+
+
+});
