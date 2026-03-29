10a9b92aec473f51758b4507c9cb9c2295aec60a
joshika-gangiredla477 <joshika.gangiredla@wsu.edu>
2026-03-29 00:46:00 +0000

    changed file name

--- concentration/scripts.js
+++ concentration/scripts.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
     //randomize cards
     let allCards = document.querySelectorAll(".card");
-    let gameboard = document.querySelector = document.querySelector("#");
+    let gameBoard = document.querySelector = document.querySelector("#");
 
     //loop through all the cards
     for (x = 0; x < allCards.length; x++) {
         let randNum = Math.floor(Math.random() * allCards.length);
-        gameboard.insertBefore(allCards[x], gameboard.children[randNum]);
+        gameBoard.insertBefore(allCards[x], gameboard.children[randNum]);
         
     }
 
