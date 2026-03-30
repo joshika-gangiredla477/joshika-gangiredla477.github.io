31b72c9ce3f1b24c5fbec0050e780a4144f07c3a
joshika-gangiredla477 <joshika.gangiredla@wsu.edu>
2026-03-30 02:31:25 +0000

    changed image links

--- concentration/scripts.js
+++ concentration/scripts.js
@@ -1,23 +1,26 @@
-//javascript for interactive concentration game for 1 player
+//javascript for inter
+
+console.log("JS LOADED!")
+
 
 //new function to flip card when clicked
 function flipCard() {
     this.classList.add("clicked");
+    console.log("clicked!")
 }
 
 //run this code when the DOM loads
-document.addEventListener("DOMContentLoaded", function(e) {
 
-    //get handles to game elements 
+document.addEventListener("DOMContentLoaded", function() {
+
     let allCards = document.querySelectorAll(".card");
-    let gameBoard = document.querySelector = document.querySelector("#gameBoard");
+    let gameBoard = document.querySelector("#gameBoard");
 
-    //randomize cards by looping through all the games cards
-    for (x = 0; x < allCards.length; x++) {
+    for (let x = 0; x < allCards.length; x++) {
         let randNum = Math.floor(Math.random() * allCards.length);
         gameBoard.insertBefore(allCards[x], gameBoard.children[randNum]);
-        
+
+        allCards[x].addEventListener("click", flipCard);
     }
 
-
-});
+}); 
