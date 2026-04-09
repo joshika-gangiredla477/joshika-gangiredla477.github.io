dd30d642a97984497434e0e292dcb1526c505448
joshika-gangiredla477 <joshika.gangiredla@wsu.edu>
2026-04-09 05:33:19 +0000

    code is updated with training data

--- weatherapp/scripts.js
+++ weatherapp/scripts.js
@@ -22,6 +22,42 @@ async function getData(url, options) {
     }
 }
 
+//update weather display in the  DOM based on the passed object
+function updateWeather(weatherObject) {
+
+
+    //output whole weather objects to console
+    console.log(weatherObject);
+
+    //update current weather temp, status, humidity
+    document.querySelector("#currentTemp span").innerHTML = weatherObject.current.temp_f;
+    document.querySelector("#currentStatus").innerHTML = weatherObject.current.condition.text;
+    document.querySelector(".humidityValue span").innerHTML = weatherObject.current.humidity;
+    
+    //output wind speed and direction in a combined string
+    let windspeed = weatherObject.current.wind_mph;
+    let winddirection = weatherObject.current.wind_dir;
+    document.querySelector(".windValue").innerHTML = windspeed + "mph " + winddirection;
+
+
+    //find all the future day blocks and loop through them, matching the forecast days in the weather obj
+    let futureDays = document.querySelectorAll(".futureDay");
+    for (let i = 0; i < futureDays.length; i++) {
+
+        //update future temp
+        futureDays[i].querySelector(".tempRange span").innerHTML = weatherObject.forecast.forecastday[i].day.maxtemp_f;
+       
+        //update future windspeed
+        windspeed = weatherObject.forecast.forecastday[i].day.maxwind_mph;
+        futureDays[i].querySelector(".wind").innerHTML = windspeed + "mph ";
+        
+        //update future condition status
+        futureDays[i].querySelector(".status").innerHTML = weatherObject.forecast.forecastday[i].day.condition.text;
+
+     }
+
+}
+
 
 
 //wait for dom to load
@@ -49,13 +85,13 @@ document.addEventListener("DOMContentLoaded", function () {
         scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
     });
 
-    let sampleURL = "https://tordevries.github.io/477/examples/ajax-api-test/current.js";
+    let sampleURL = "https://tordevries.github.io/477/examples/ajax-api-test/current-forecast.js";
     let sampleOptions = {}
 
     //get sample data
     getData(sampleURL, sampleOptions).then(function (result) {
         // code to operate on “result” JSON object
-        console.log(result.current.temp_f);
+        updateWeather(result);
     });
 
 
